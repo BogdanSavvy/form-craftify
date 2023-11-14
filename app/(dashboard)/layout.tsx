e43118ed6200fb48b04ee44abe4af6a98058361a
BogdanSavvy <bogdansavin315@gmail.com>
2023-11-14 14:08:16 +0200

    Added shadcn-ui components, added theaming, created nav

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
+import Logo from '@/components/logo';
+import ThemeSwither from '@/components/theme-switcher';
+import { UserButton } from '@clerk/nextjs';
 
 export default function DashboardLayout({
 	children,
@@ -7,9 +9,12 @@ export default function DashboardLayout({
 }) {
 	return (
 		<div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-			<nav>
-				NAVIGATION
-				<UserButton afterSignOutUrl="/sign-in" />
+			<nav className="flex justify-between items-center border-b border-border h-[80px] px-4 py-2">
+				<Logo />
+				<div className="flex gap-4">
+					<ThemeSwither />
+					<UserButton afterSignOutUrl="/sign-in" />
+				</div>
 			</nav>
 			<main className="flex flex-grow w-full">{children}</main>
 		</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 
+import { ThemeProvider } from '@/components/providers/theme-provider';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -10,11 +12,19 @@ export const metadata: Metadata = {
 	description: 'Create forms easily',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+						{children}
+					</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);

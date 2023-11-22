8c63385b6b802325624fbd18d929cec7e82d6a0b
BogdanSavvy <bogdansavin315@gmail.com>
2023-11-22 21:16:11 +0200

    added all dnd conditions in application

--- components/designer.tsx
+++ components/designer.tsx
@@ -10,8 +10,13 @@ import { ElementsType, FormElements } from '@/components/form-elements';
 import { DesignerElementWrapper } from '@/components/designer-element-wrapper';
 
 export const Designer = () => {
-	const { elements, addElement, selectedElement, setSelectedElement } =
-		useDesigner();
+	const {
+		elements,
+		addElement,
+		selectedElement,
+		setSelectedElement,
+		removeElement,
+	} = useDesigner();
 
 	const droppable = useDroppable({
 		id: 'designer-drop-area',
@@ -30,12 +35,74 @@ export const Designer = () => {
 
 			const isDesignerButton = active.data?.current?.isDesignerButton;
 
-			if (isDesignerButton) {
+			const isDropedOverDesignerDropArea =
+				over.data?.current?.isDesignerDropArea;
+
+			//? first condition of dropping elements in DesignerComponent
+			if (isDesignerButton && isDropedOverDesignerDropArea) {
 				const type = active.data?.current?.type;
 				const newElement = FormElements[type as ElementsType].construct(
 					idGenerator(),
 				);
 				addElement(0, newElement);
+				return;
+			}
+
+			const isDropedOverDesignerElementTopHalf =
+				over.data?.current?.isTopHalfOfElement;
+			const isDropedOverDesignerElementBottomHalf =
+				over.data?.current?.isBottomHalfOfElement;
+
+			const isDropedOverDesignerElement =
+				isDropedOverDesignerElementTopHalf ||
+				isDropedOverDesignerElementBottomHalf;
+
+			//? second condition of dropping elements in DesignerComponent
+			if (isDesignerButton && isDropedOverDesignerElement) {
+				const type = active.data?.current?.type;
+				const newElement = FormElements[type as ElementsType].construct(
+					idGenerator(),
+				);
+
+				const overId = over.data?.current?.elementId;
+
+				const overElementIndex = elements.findIndex(el => el.id === overId);
+				if (overElementIndex === -1) {
+					throw new Error('Element not found');
+				}
+
+				let indexForNewElement = overElementIndex;
+				if (isDropedOverDesignerElementBottomHalf) {
+					indexForNewElement = overElementIndex + 1;
+				}
+
+				addElement(indexForNewElement, newElement);
+				return;
+			}
+
+			//? third condition of dropping elements in DesignerComponent
+			const isDraggingElement = active.data?.current?.isDesignerElement;
+
+			if (isDraggingElement && isDropedOverDesignerElement) {
+				const activeId = active.data?.current?.elementId;
+				const overId = over.data?.current?.elementId;
+
+				const activeElementIndex = elements.findIndex(el => el.id == activeId);
+				const overElementIndex = elements.findIndex(el => el.id == overId);
+
+				if (activeElementIndex === -1 || overElementIndex == -1) {
+					throw new Error('element not found');
+				}
+
+				const activeElement = { ...elements[activeElementIndex] };
+				removeElement(activeId);
+
+				let indexForNewElement = overElementIndex;
+				if (isDropedOverDesignerElementBottomHalf) {
+					indexForNewElement = overElementIndex + 1;
+				}
+
+				addElement(indexForNewElement, activeElement);
 			}
 		},
 	});
@@ -53,8 +120,8 @@ export const Designer = () => {
 				<div
 					ref={droppable.setNodeRef}
 					className={cn(
-						'h-full max-w-[920px] m-auto flex flex-col flex-grow flex-1 items-center justify-start bg-background overflow-y-auto',
-						droppable.isOver && 'ring-2 ring-primary/20',
+						'h-full max-w-[920px] m-auto flex flex-col flex-grow flex-1 items-center justify-start rounded-xl bg-background overflow-y-auto',
+						droppable.isOver && 'ring-2 ring-primary ring-inset',
 					)}
 				>
 					{!droppable.isOver && elements.length === 0 && (
